25bf5a803b779fa5bc1df4ef5da442d753711f44
Daikichi1223 <abcdaiki1223@icloud.com>
2021-02-16 11:06:02 +0900

    マイグレーションファイルの編集

--- db/migrate/20210216014442_create_meals.rb
+++ db/migrate/20210216014442_create_meals.rb
@@ -1,6 +1,10 @@
 class CreateMeals < ActiveRecord::Migration[6.0]
   def change
     create_table :meals do |t|
+      t.string :name,         null: false
+      t.text :explanation,    null: false
+      t.integer :category,    null: false
+      t.integer :price,       null: false
       t.timestamps
     end
   end

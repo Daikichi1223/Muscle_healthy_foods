affd4ebf3920f0f78e7017391fa7497a7b94572e
Daikichi1223 <abcdaiki1223@icloud.com>
2021-02-25 11:09:54 +0900

    モデルの単体テスト

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -6,6 +6,7 @@ class Meal < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do 
+    validates :image, presence: { message: "を選択してください" }
     validates :name
     validates :explanation
     validates :protein
@@ -14,5 +15,9 @@ class Meal < ApplicationRecord
     validates :price
   end
 
-  validates :genre_id, numericality: { other_than: 1 }
+  validates :genre_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :name, length: { maximum: 40 }
+  validates :explanation, length: { maximum: 1000 }
+  validates :price, presence: true, inclusion: { in: 1..9999999, message: "は1円~9,999,999円の範囲で入力してください"}
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
 end
